dea8629b0392473fb0a0cf3e3d633f4dee7a5fd6
Jbudd512 <Jbudd0512@gmail.com>
2022-01-15 13:53:22 -0600

    week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,52 +10,171 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+// 1a) Pseudo Code:
+// Copy and paste test code template into assignment (make sure nothing is inside initial template)
+// Read problem statement and understand inputs/outputs (inputs need to be directly inserted into test code)
+// Create relevant info for describe, it, expect, and toEqual methods
+// Test code output to ensure it is failing red (should get a reference error saying function is not defined)
 
-
+describe("l33t1f1c4t10n", () => {
+  it("a function which takes in a string argument and returns a similar string, though has most of the vowels replaced with their l33t-speak equivalents", () => {
+    expect(l33t1f1c4t10n("Lackadaisical")).toEqual("L4ck4d41s1c4l");
+    expect(l33t1f1c4t10n("Gobbledygook")).toEqual("G0bbl3dyg00k");
+    expect(l33t1f1c4t10n("Eccentric")).toEqual("3cc3ntr1c");
+  });
+});
 
 // b) Create the function that makes the test pass.
 
+// 1b) Pseudo Code:
+// Declare a function and name it l33t1f1c4t10n (const declaration)
+// Name the parameter as string
+// Declare a variable, name it stringArray (let declaration), and initialize it to string.split("")
+// Apply a .filter HOF, which will iterate through all elements of stringArray and apply conditionals, which subsitute l33t-speak numbers in place of any a, e, i, and o (4, 3, 1, and 0 respectively)
+// Apply a .join("") to the end of the filter. Also, return the whole expression (ie stringArray.filter.join(""))
+// Run the tests to ensure it passes
 
+const l33t1f1c4t10n = (string) => {
+  let stringArray = string.split("");
+  let stringArrayNew = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i].toLowerCase() === "a") {
+      stringArrayNew.push("4");
+    } else if (stringArray[i].toLowerCase() === "e") {
+      stringArrayNew.push("3");
+    } else if (stringArray[i].toLowerCase() === "i") {
+      stringArrayNew.push("1");
+    } else if (stringArray[i].toLowerCase() === "o") {
+      stringArrayNew.push("0");
+    } else {
+      stringArrayNew.push(stringArray[i]);
+    }
+  }
+  return stringArrayNew.join("");
+};
+
+// Notes: I initially ran the test, and it failed with the correct error. As I was writing my actual code, I realized .filter could not be used the way I was thinking it could be used (at least I wasn't able to get it to work properly). So I decided to go with what I know and write a for loop. I also had to add a .toLowerCase() to include capital letters in the conditionals. I also needed a second variable to .push letters and numbers into. I ran the test again and it passed green. The only refactor I would do is try to find an HOF to make this work, but .filter didn't seem to work for me. Maybe .map could work? Also, might be able to use ternary operators here. I will attempt to refactor this if time permits.
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-const letterA = "a"
-// Expected output: ["Apple", "Banana", "Orange"]
-const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+// 2a) Pseudo Code:
+// Copy and paste test code template into assignment (make sure nothing is inside initial template)
+// Read problem statement and understand inputs/outputs (inputs need to be directly inserted into test code)
+// Create relevant info for describe, it, expect, and toEqual methods
+// Test code output to ensure it is failing red (should get a reference error saying function is not defined)
 
-
+describe("charSearch", () => {
+  it("A function which takes in an array of strings and a string character, and returns a new array of strings which contain that character", () => {
+    expect(
+      charSearch(["Apple", "Banana", "Plum", "Orange", "Kiwi"], "a")
+    ).toEqual(["Apple", "Banana", "Orange"]);
+    expect(
+      charSearch(["Mango", "Cherry", "Apricot", "Blueberry", "Peach"], "e")
+    ).toEqual(["Cherry", "Blueberry", "Peach"]);
+  });
+});
 
 // b) Create the function that makes the test pass.
 
+// 2b) Pseudo Code:
+// Declare a function and name it charSearch (const declaration)
+// Name the parameters as array and character respectively
+// Create a return statement, which performs a .filter on the array parameter
+// .filter logic
+//      value.toLowerCase().includes(character)
+// Run the test code to ensure it is passing
 
+const charSearch = (array, character) => {
+  return array.filter((value) => value.toLowerCase().includes(character));
+};
+
+// Notes: I initially ran the test, got the correct failure. As for writing the code, this one was pretty straightforward for me. I honestly can't even think of a refactor for this. Just very simple code that I actually like.
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
+// 3a) Pseudo Code:
+// Copy and paste test code template into assignment (make sure nothing is inside initial template)
+// Read problem statement and understand inputs/outputs (inputs need to be directly inserted into test code)
+// Create relevant info for describe, it, expect, and toEqual methods
+// Test code output to ensure it is failing red (should get a reference error saying function is not defined)
 
-
+describe("bobSaget", () => {
+  it("A function that takes in an array and returns true if the resulting 'hand' is a full house, and otherwise returns false", () => {
+    expect(bobSaget([5, 5, 5, 3, 3])).toEqual(true);
+    expect(bobSaget([5, 5, 3, 3, 4])).toEqual(false);
+    expect(bobSaget([5, 5, 5, 5, 4])).toEqual(false);
+  });
+});
 
 // b) Create the function that makes the test pass.
+
+// 3b) Pseudo code:
+// Declare a function and name it bobSaget (const declaration)
+// Name the parameter as array
+// Declare a variable, name it as sortArray, and initialize it as array.sort() (let declaration)
+// Declare another variable, name it as counterArray, and initialize it as an empty array (let declaration)
+// Create a for loop,  which iterates through all elements of the sortArray () (use i as the incrementer for the loop count, initialized to 0)
+// For loop logic:
+//       Create a counter variable (let declaration) with an initial value of 0
+//       Create a nested for loop (use j as the incrementer for the nested loop count, and set the initial value to 0)
+//       Nested for loop logic:
+//              Evaluate whether or not the value at the ith index equals that of the value at the jth index. If they are equal, increment the counter variable. However, if they do not equal, set the value of i to that of the current value of j. Also set the value of j equal to the length of sortArray to terminate the nested loop early
+//       .push the value of the counter variable into the counterArray
+// Create conditionals which evaluate the entries of counterArray, such that if either counterArray[0] === 3 && counterArray[1] === 2 || counterArray[0] === 2 && counterArray[1] === 3, it returns true. Otherwise have it return false
+
+// const bobSaget = (array) => {
+//   let sortArray = array.sort();
+//   console.log(sortArray);
+//   let counterArray = [];
+//   for (let i = 0; i < sortArray.length; i++) {
+//     let counter = 0;
+//     for (let j = i; j < sortArray.length; j++) {
+//       console.log(i, "i before");
+//       console.log(j, "j before");
+//       if (sortArray[i] === sortArray[j]) {
+//         counter++;
+//         console.log(counter, "counter");
+//       } else if (sortArray[i] !== sortArray[j]) {
+//         i = j - 1;
+//         j = sortArray.length;
+//         console.log(i, "i after");
+//         console.log(j, "j after");
+//       } else if (j === sortArray.length - 1) {
+//         i = j - 1;
+//         j = sortArray.length;
+//       }
+//     }
+//     counterArray.push(counter);
+//     console.log(counterArray);
+//   }
+//   if (
+//     (counterArray[0] === 3 && counterArray[1] === 2) ||
+//     (counterArray[0] === 2 && counterArray[1] === 3)
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+const bobSaget = (array) => {
+  let sortArray = array.sort();
+  if (
+    (sortArray[0] === sortArray[2] && sortArray[3] === sortArray[4]) ||
+    (sortArray[0] === sortArray[1] && sortArray[2] === sortArray[4])
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Notes: I ran the initial test and it failed with the appropriate error message. And then...after hours of swearing and trying to figure out how to make this work with for loops, I realized purely using conditionals was by far the easier method. This works mainly because a full house condition is easy to find once you use .sort() on the input array. I honestly can't (and don't want to) think of a refactor for this, simply because this gave me a massive headache. I left the original code commented out to show the monster I was working with.
--- interview-questions.md
+++ interview-questions.md
@@ -1,4 +1,5 @@
 # ASSESSMENT 5: Interview Practice Questions
+
 Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory.
@@ -7,53 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a block in Ruby?
 
-  Your answer:
+Your answer: Ruby blocks contain the logic for anonymous functions within methods. This is pretty much all I know, I won't claim I understand them fully.
 
-  Researched answer:
-
-
+Researched answer: Ruby blocks are the actual anonymous functions which can be passed into methods. They are usually enclosed by a do-end statement or curly braces. They can have arguments, which are contained between pipes (||).
 
 2. What is a gem?
 
-  Your answer:
+Your answer: A gem is a package, or group of depdendencies, for Ruby which enables greater functionality. Gems are generally open-source (convenient since Ruby is also open-source). They are installed via RubyGems, a package manager.
 
-  Researched answer:
-
-
+Researched answer: Ruby Gems are open-source, the format for which is governed by a standard code structure which includes the lib directory (where the code is actually stored), the test or spec directory (contains tests for the code), usually a Rakefile (automatically runs tests and generates code), an executable file in the bin directory, a readme file, and the gemspec, which contains pertinent info about the gem itself.
 
 3. What is Ruby on Rails?
 
-  Your answer:
+Your answer: Ruby on Rais is a full-stack framework based on the Ruby language, which offers a quick means of making a full stack website. In place of versatility, Rails emphasizes specialization and quick turn around time for web development. It uses the Model-View-Controller layout for website development. It is often paired with other OOP-based languages, since Ruby is entirely OOP-based.
 
-  Researched answer:
-
-
+Researched answer: There are a number of noteable websites which are written in Rails: Github, Kickerstarter (kind of a scam website now but still noteworthy), Twitch, Instacart, Soundcloud, etc.
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+Your answer: A relational database is a database where information is stored inside of instances, with different pieces of information pertaining to said instances being organized by named columns. They require primary keys so that there is a unique identifier for each instance, which can be used for manipulating or reading data (or any other CRUD action). I would assume there are other types of databases but I'm not sure what they are.
 
-  Researched answer:
-
-
+Researched answer: A relational database, or a Relational Database Management System (RDBMS), is a collection of data items with pre-defined relationships between them. Items are organized by columns and rows. They are based on the relational model, which emphasizes maintaining data consistency across applications and database copies (or instances). There are four properties which define relational databases (under the acroynm A.C.I.D): Atomicity, Consistency, Isolation, and Durability. Atomicity defines all of the elements that make up a complete database transaction. Consistency defines the rules for maintaining data points in the correct state after a transaction. Isolation keeps the effect of a transaction invisible to other until it is committed. Durability ensures that data changes become permanent once the transaction is committed. The other kind of database, called a non-relational database, does not use any tables, rows, or primary keys to store data. Each database is customized depending on the dataset being stored.
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+Your answer: Primary keys are unique identifiers for instances within a relational database. They are needed so that there is a way to reference any given instance w/o having any redundancy. They are especially useful for updating individual instances. A database can have as many primary keys as storage space allows.
 
-  Researched answer:
-
-
+Researched answer: The Active Record, in Rails, create a column naned id which is used for storing primary keys. It defaults to being an integer value, though it can be changed in Rails. UUIDs are a viable option to use in place of integers. They are random strings in a predefined format, which are considered more secure than integers.
 
 ## Looking Ahead: Terms for Next Week
+
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: REST stands for Representational State Transfer. In following this convention, we termed the phrase RESTful routes, the convention for which is used to map HTTP methods and CRUD actions together. There are 7 RESTful routes: Index, which displays a list of all websites; New, which shows the form to create a new website; Create, which adds a new website to the database; Show, which shows info about the websites; Edit, which shows the edit form for a website; Update, which updates a website; and Destroy, which deletes a website.
 
-2. JSON:
+2. JSON: JSON, which stands for Javascript Object Notation, is an opne standard file format and data interchange format which uses human-readable text to store and transmit data objects consisting of attribute-value pairs and arrays. It is typically used for transmitting data in web applications.
 
-3. ERB:
+3. ERB: ERB, which stands for Embedded RuBy, is a program in Rails which generates HTML files with Ruby code embedded inside. These HTML files can be rendered front-end web pages, and so servers as a link between the front-end and the back-end of the application.
 
-4. Params:
+4. Params: Params is short for parameter, which is a placeholder within a method or a function used to pass arguements to the code block in order to create logical evaluations.
 
-5. API:
+5. API: API stands for Application Programming Interface. It is a software intermediary which allows two applications to talk to each other. Some examples of uses for APIs include websites which collect data from other websites for display (kayak.com comes to mind), fingerprint authentication, etc.
